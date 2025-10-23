f4ed1836146404b22ba98e41ecd2729027ffe5a3
Codingwithshayan <shayanayan710@gmail.com>
2025-10-24 02:09:06 +0500

    If else

--- main.dart
+++ main.dart
@@ -1,10 +1,20 @@
 void main(){
-    print("Hello World");
-    
-    int a = 10;
-    int b = 20;
-    print(a<b && b>a);
-    print(b>a || a<b);
-    print(!(a>b) && b>a);
-
+   // Total marks = 50
+int num01= 8;
+int num02= 7;
+int num03 = 10;
+int num04 = 9;
+int num05 = 9;
+ int Marks = (num01+num02+num03+num04+num05);
+print("Marks Obtained = $Marks");
+int num07 = 69;
+int num08 = 90;
+  double Attandance = (num07*100/num08);
+  print("Attandance% = $Attandance");
+  
+  if(Marks>40 && Attandance>75){
+    print("PASS");
+  }else if(Marks<40 && Attandance<75){
+    print("FAIL");
+  }
 }
